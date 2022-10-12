a9e20b1d92b739318434dfefd78c0d785fefdf9c
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-12 10:36:07 +0530

    Added dynamic data using props

--- starting_with_react/01-starting-setup/01-starting-setup/src/App.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/App.js
@@ -1,8 +1,44 @@
 import ExpenseItem from './components/ExpenseItem'
+const expenses = [
+  {
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    locationOfExpenditure : 'Mumbai'
+  },
+  { 
+     title: 'New TV', 
+     amount: 799.49, 
+     date: new Date(2021, 2, 12),
+     locationOfExpenditure : 'Mumbai' },
+  {
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    locationOfExpenditure : 'Bangalore'
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    locationOfExpenditure : 'Pune'
+  },
+];
 function App() {
+  let expCompArr = [];
+  for(let i=0;i<expenses.length;i++){
+    expCompArr.push(
+    <ExpenseItem title= {expenses[i].title}
+    amount= {expenses[i].amount}
+    date= {expenses[i].date}
+    location={expenses[i].locationOfExpenditure}>
+    </ExpenseItem>)
+  }   
   return (
     <div>
       <h2>Let's get started!</h2>
+      {expCompArr}
     </div>
   );
 }
--- starting_with_react/01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const exp_date = new Date(2022, 3, 18).toISOString();
-    const exp_title = "Car Insurance";
-    const exp_price = 294.16;
-    const locationOfExpenditure = "Mumbai";
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{exp_date}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{exp_title}
+                <h2>{props.title}
                     {" "}
-                    Location : {locationOfExpenditure}</h2>
-                <div className="expense-item__price">${exp_price}</div>
+                    Location : {props.location}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
