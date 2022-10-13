b19b65fa7d6f609a169698c8f2ee4392453b6818
sarvesh-7 <sarveshgawade42@gmail.com>
2022-10-13 09:21:09 +0530

    Task 10 - Props manipulation to display date in readable format

--- starting_with_react/01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
+++ starting_with_react/01-starting-setup/01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    let month = props.date.toLocaleString('en-US', { month : 'long' });
+    let day = props.date.toLocaleString('en-US', {day : '2-digit' });
+    let year = props.date.getFullYear();
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
+            </div>
+           
             <div className="expense-item__description">
                 <h2>{props.title}
                     {" "}
